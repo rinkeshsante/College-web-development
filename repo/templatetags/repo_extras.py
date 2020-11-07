982638f6dab2fc9ca58f117cc00f5c57349293c7
RinkeshSante <rinkoosante@gmail.com>
2020-11-07 17:32:34 +0530

    hiding extra buttons

--- repo/templatetags/repo_extras.py
+++ repo/templatetags/repo_extras.py
@@ -1,11 +1,21 @@
 from django import template
-from ..services import get_user_dep
+from ..services import get_user_dep, is_sub_admin, is_authorized
 register = template.Library()
 
 
 @register.filter
 def getValue(value, arg):
     return getattr(value, arg)
+
+
+@register.filter
+def isSubAdmin(value):
+    return is_sub_admin(value)
+
+
+@register.filter
+def isAuthorized(value):
+    return is_authorized(value)
 
 
 @register.filter
