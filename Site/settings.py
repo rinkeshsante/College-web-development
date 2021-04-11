df110eb4fa6ecd0c0a28fc3b91d0c52e11c4c915
RinkeshSante <rinkoosante@gmail.com>
2021-04-11 08:17:25 +0530

    added static files to project

--- Site/settings.py
+++ Site/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
---
+++ static/js/myscript.js
@@ -0,0 +1,39 @@
+let button = document.querySelector(".btn-theme-toggle");
+if (localStorage.clickcount % 2 == 0) {
+  document.documentElement.classList.toggle("dark-mode");
+}
+button.addEventListener("click", () => {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount) + 1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.documentElement.classList.toggle("dark-mode");
+});
+
+$(document).ready(function () {
+  $("#dataTable").DataTable({
+    initComplete: function () {
+      this.api()
+        .columns()
+        .every(function () {
+          var column = this;
+          var select = $('<select><option value=""></option></select>')
+            .appendTo($(column.footer()).empty())
+            .on("change", function () {
+              var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+              column.search(val ? "^" + val + "$" : "", true, false).draw();
+            });
+
+          column
+            .data()
+            .unique()
+            .sort()
+            .each(function (d, j) {
+              select.append('<option value="' + d + '">' + d + "</option>");
+            });
+        });
+    },
+  });
+});
